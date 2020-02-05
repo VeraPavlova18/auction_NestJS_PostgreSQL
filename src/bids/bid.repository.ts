b897fc6e9f4509f56b958e7476b1c03baf33b6b2
vera <pavlova.vera18@gmail.com>
2020-02-05 17:19:16 +0200

    configured sql transaction for bid creating process and added validation for proposed price

--- src/bids/bid.repository.ts
+++ src/bids/bid.repository.ts
@@ -1,26 +1,70 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, getManager } from 'typeorm';
 import { User } from '../auth/user.entity';
-import { Logger, InternalServerErrorException } from '@nestjs/common';
+import { Logger, InternalServerErrorException, BadRequestException } from '@nestjs/common';
 import { Bid } from './bid.entity';
 import { CreateBidDto } from './dto/create-bid.dto';
 import * as moment from 'moment';
 import { BidCustomer } from './bidCustomer.interface';
+import { Lot } from 'src/lots/lot.entity';
+import { LotStatus } from 'src/lots/lot-status.enum';
 
 @EntityRepository(Bid)
 export class BidRepository extends Repository<Bid> {
   private logger = new Logger('BidRepository');
 
-  async createBid(user: User, createBidDto: CreateBidDto, id: number): Promise<BidCustomer> {
+  customizeBid(bid: Bid, user: User): BidCustomer {
+    const customer = (bid.userId === user.id) ? `You` : `Customer ${Math.floor(Math.random() * 100000 + 1)}`;
+    delete bid.user;
+    delete bid.userId;
+    return { ...bid, customer } as BidCustomer;
+  }
+
+  async createBid(
+    user: User,
+    createBidDto: CreateBidDto,
+    id: number,
+  ): Promise<BidCustomer> {
     const { proposedPrice } = createBidDto;
-
-    const bid = new Bid();
-    bid.proposedPrice = proposedPrice;
-    bid.creationTime = moment().toDate();
-    bid.user = user;
-    bid.lotId = id;
-
     try {
-      await bid.save();
+      return getManager().transaction(
+        'SERIALIZABLE',
+        async transactionalEntityManager => {
+          await transactionalEntityManager.query(
+            'LOCK TABLE bid IN ACCESS EXCLUSIVE MODE;',
+          );
+          const { max } = await transactionalEntityManager
+            .createQueryBuilder(Bid, 'bid')
+            .select('Max(bid.proposedPrice)', 'max')
+            .where('bid.lotId = :lotId', { lotId: id })
+            .getRawOne();
+          const lot = await transactionalEntityManager.findOne(Lot, id);
+          const { estimatedPrice, curentPrice } = lot;
+
+          if (proposedPrice < curentPrice || proposedPrice > estimatedPrice ) {
+            throw new BadRequestException({
+              status: 400,
+              error: `proposedPrice: ${proposedPrice} must be equal or greater then lot curentPrice: ${curentPrice} and less or equal then estimatedPrice: ${estimatedPrice}`,
+            });
+          }
+          if (proposedPrice <= max || proposedPrice > estimatedPrice) {
+            throw new BadRequestException({
+              status: 400,
+              error: `proposedPrice: ${proposedPrice} must be greater then previous bid: ${max} and less or equal then estimatedPrice: ${estimatedPrice}`,
+            });
+          }
+          if (+proposedPrice === estimatedPrice) {
+            lot.status = LotStatus.CLOSED;
+            await transactionalEntityManager.save(lot);
+          }
+          const bid = new Bid();
+          bid.proposedPrice = proposedPrice;
+          bid.creationTime = moment().toDate();
+          bid.user = user;
+          bid.lotId = id;
+          await transactionalEntityManager.save(bid);
+          return this.customizeBid(bid, user);
+        },
+      );
     } catch (error) {
       this.logger.error(
         `Failed to create a Bid for user ${user.email}. Data: ${createBidDto}`,
@@ -28,40 +72,15 @@ export class BidRepository extends Repository<Bid> {
       );
       throw new InternalServerErrorException();
     }
-    let customer;
-    if (bid.userId === user.id) {
-      customer = `You`;
-    } else {
-      customer = `Customer ${Math.floor(Math.random() * 100000 + 1)}`;
-    }
-    delete bid.user;
-    delete bid.userId;
-    return {
-      ...bid,
-      customer,
-    } as BidCustomer;
   }
 
   async getBids(user: User, id: number): Promise<BidCustomer[]> {
     const query = this.createQueryBuilder('bid');
-    let customer;
     try {
       const bids = await query
         .where('bid.lotId = :lotId', { lotId: id })
         .getMany();
-
-      return bids.map((bid) => {
-        if (bid.userId === user.id) {
-          customer = `You`;
-        } else {
-          customer = `Customer ${Math.floor(Math.random() * 100000 + 1)}`;
-        }
-        delete bid.userId;
-        return {
-          ...bid,
-          customer,
-        };
-      }) as BidCustomer[];
+      return bids.map(bid => this.customizeBid(bid, user));
     } catch (error) {
       this.logger.error(
         `Failed to get bids for user "${user.email}".`,
