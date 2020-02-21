dd559b8cd85387dafd277b0e4b557b1c8a963c77
vera <pavlova.vera18@gmail.com>
2020-02-21 17:28:07 +0200

    feat: add tests for bid creating and getting

--- test/bids.e2e-spec.ts
+++ test/bids.e2e-spec.ts
@@ -1,5 +1,4 @@
 import { UserRepository } from '../src/auth/user.repository';
-import { users, lots } from './constants';
 import { createTestingAppModule } from './config/testingmodule-config';
 import { LotRepository } from '../src/lots/lot.repository';
 import { BidRepository } from '../src/bids/bid.repository';
@@ -14,6 +13,7 @@ describe('LotsController (e2e)', () => {
   let accessToken2;
   let lotsExist;
   let client;
+  let bidsExist;
 
   beforeAll(async () => {
     const init = await createTestingAppModule();
@@ -40,16 +40,106 @@ describe('LotsController (e2e)', () => {
         .expect(401);
     });
 
-    it('should create a bid for authorization user1 in the DB', async () => {
+    it('should not create a bid with proposedPrice < lot.curentPrice', async () => {
+      await client
+        .post(`/lots/${lotsExist[0].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken1}`)
+        .send({ proposedPrice: 2 })
+        .expect(400)
+        .expect(({ body }) => {
+          expect(body.error).toEqual('proposedPrice: 2 must be equal or greater then lot curentPrice: 5 and less or equal then estimatedPrice: 555');
+        });
+    });
+
+    it('should not create a bid with proposedPrice < 0', async () => {
+      await client
+        .post(`/lots/${lotsExist[0].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken1}`)
+        .send({ proposedPrice: -50 })
+        .expect(400)
+        .expect(({ body }) => {
+          expect(body.error).toEqual('Bad Request');
+        });
+    });
+
+    it('should create a bid in the lot1 for authorization user1 in the DB', async () => {
       await client
         .post(`/lots/${lotsExist[0].id}/bids`)
         .set('Authorization', `Bearer ${accessToken1}`)
         .send({ proposedPrice: 25 })
         .expect(201);
 
-      const bidsExist = await bidRepository.query(`SELECT * FROM "bid"`);
+      bidsExist = await bidRepository.query(`SELECT * FROM "bid"`);
       expect(bidsExist[0].lotId).toEqual(lotsExist[0].id);
       expect(bidsExist[0].proposedPrice).toEqual(25);
+    });
+
+    it('should not create a bid with same proposedPrice', async () => {
+      await client
+        .post(`/lots/${lotsExist[0].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken1}`)
+        .send({ proposedPrice: 25 })
+        .expect(400)
+        .expect(({ body }) => {
+          expect(body.error).toEqual('proposedPrice: 25 must be greater then previous bid: 25 and less or equal then estimatedPrice: 555');
+        });
+    });
+
+    it('should create second bid in the lot1 for authorization user1 in the DB', async () => {
+      await client
+        .post(`/lots/${lotsExist[0].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken1}`)
+        .send({ proposedPrice: 27 })
+        .expect(201);
+
+      bidsExist = await bidRepository.query(`SELECT * FROM "bid"`);
+      expect(bidsExist[1].lotId).toEqual(lotsExist[0].id);
+      expect(bidsExist[1].proposedPrice).toEqual(27);
+    });
+
+    it('should create a bid in lot1 for authorization user2 in the DB', async () => {
+      await client
+        .post(`/lots/${lotsExist[0].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken2}`)
+        .send({ proposedPrice: 28 })
+        .expect(201);
+
+      bidsExist = await bidRepository.query(`SELECT * FROM "bid"`);
+      expect(bidsExist[2].lotId).toEqual(lotsExist[0].id);
+      expect(bidsExist[2].proposedPrice).toEqual(28);
+    });
+
+    it('should create a bid in lot2 for authorization user2 in the DB', async () => {
+      await client
+        .post(`/lots/${lotsExist[1].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken2}`)
+        .send({ proposedPrice: 10 })
+        .expect(201);
+
+      bidsExist = await bidRepository.query(`SELECT * FROM "bid"`);
+      expect(bidsExist[3].lotId).toEqual(lotsExist[1].id);
+      expect(bidsExist[3].proposedPrice).toEqual(10);
+    });
+
+    it('should not create a bid in lot2 if lot status is not equal to "IN_PROCESS"', async () => {
+      // change lots status
+      await lotRepository.query(`UPDATE "lot" SET "status" = 'CLOSED'`);
+
+      await client
+        .post(`/lots/${lotsExist[1].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken2}`)
+        .send({ proposedPrice: 10 })
+        .expect(400);
+
+      // change lots status
+      await lotRepository.query(`UPDATE "lot" SET "status" = 'PENDING'`);
+
+      await client
+        .post(`/lots/${lotsExist[1].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken2}`)
+        .send({ proposedPrice: 10 })
+        .expect(400);
+
     });
   });
 
@@ -60,11 +150,34 @@ describe('LotsController (e2e)', () => {
         .expect(401);
     });
 
-    it('should return bids for authorization user1', async () => {
+    it('should return bids in lot1 for authorization user1', async () => {
       await client
         .get(`/lots/${lotsExist[0].id}/bids`)
         .set('Authorization', `Bearer ${accessToken1}`)
-        .expect(200);
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.length).toEqual(3);
+        });
+    });
+
+    it('should return bids in lot1 for authorization user2', async () => {
+      await client
+        .get(`/lots/${lotsExist[0].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken2}`)
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.length).toEqual(3);
+        });
+    });
+
+    it('should return bids in lot2 for authorization user2', async () => {
+      await client
+        .get(`/lots/${lotsExist[1].id}/bids`)
+        .set('Authorization', `Bearer ${accessToken2}`)
+        .expect(200)
+        .expect(({ body }) => {
+          expect(body.length).toEqual(1);
+        });
     });
   });
 
